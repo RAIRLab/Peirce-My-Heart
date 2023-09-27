618c36d8c0097e74805084319bb070997fd08a6a
Ryan Reilly <ryanreilly703@gmail.com>
2023-09-27 09:45:46 -0400

    Should no longer break master? Changed returns

--- src/AEG/Ellipse.ts
+++ src/AEG/Ellipse.ts
@@ -70,7 +70,7 @@ export class Ellipse {
      */
     public containsPoint(otherPoint: Point): boolean {
         //ELLIPSE TO BE IMPLEMENTED ACCURATELY
-        return this.boundingBox.containsPoint(otherPoint);
+        return false;
     }
 
     /**
@@ -80,7 +80,7 @@ export class Ellipse {
      */
     public overlaps(otherShape: Rectangle | Ellipse): boolean {
         //ELLIPSE TO BE IMPLEMENTED ACCURATELY
-        return this.boundingBox.overlaps(otherShape);
+        return false;
     }
 
     /**
@@ -90,6 +90,6 @@ export class Ellipse {
      */
     public containsShape(otherShape: Rectangle | Ellipse): boolean {
         //ELLIPSE TO BE IMPLEMENTED ACCURATELY
-        return this.boundingBox.containsShape(otherShape);
+        return false;
     }
 }
