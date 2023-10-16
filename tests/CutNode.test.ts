6e6ad524907b3089057cd752bba47b469e7fc6eb
Ryan Reilly <ryanreilly703@gmail.com>
2023-10-16 10:44:12 -0400

    getCurrentCut() tests CutNode.test.ts - tests for same level, one level, and two level deep placements on getCurrentCut()

--- tests/CutNode.test.ts
+++ tests/CutNode.test.ts
@@ -6,9 +6,10 @@ import {Ellipse} from "../src/AEG/Ellipse";
 import {Point} from "../src/AEG/Point";
 
 const testCenter = new Point(5, 5);
+const testEllipse = new Ellipse(testCenter, 5, 5);
 
 describe("CutNode constructor soliloquy:", () => {
-    const cNode: CutNode = new CutNode(new Ellipse(testCenter, 5, 5));
+    const cNode: CutNode = new CutNode(testEllipse);
 
     test("CutNode constructor with only one argument passed in should produce a child list of length 0.", () => {
         expect(cNode.children.length).toBe(0);
@@ -65,8 +66,45 @@ describe("CutNode constructor soliloquy:", () => {
     );
 });
 
-describe.skip("CutNode getCurrentCut soliloquy:", () => {
-    const cNode: CutNode = new CutNode(new Ellipse(new Point(5, 5), 5, 5));
+describe("CutNode getCurrentCut soliloquy:", () => {
+    const cNode: CutNode = new CutNode(null);
+    const jay: AtomNode = new AtomNode("J", new Point(6, 6), 3, 3);
+
+    test("getCurrentCut should return the CutNode it was called on if that CutNode has no children.", () => {
+        expect(cNode.getCurrentCut(jay)).toStrictEqual(cNode);
+    });
+
+    test("getCurrentCut should return the CutNode it was called on if that CutNode only has children of type AtomNode.", () => {
+        cNode.child = new AtomNode("X", testCenter, 1, 1);
+        cNode.child = new AtomNode("Y", testCenter, 2, 2);
+        cNode.child = new AtomNode("Z", testCenter, 3, 3);
+
+        expect(cNode.getCurrentCut(jay)).toStrictEqual(cNode);
+    });
+
+    test("getCurrentCut should return the CutNode it was called on if a node of that type exists in its child list, but is not larger than the node sent as an argument.", () => {
+        const cNodeChildCutSmaller: CutNode = new CutNode(new Ellipse(testCenter, 1, 1));
+        cNode.child = cNodeChildCutSmaller;
+
+        expect(cNode.getCurrentCut(jay)).toStrictEqual(cNode);
+    });
+
+    test("getCurrentCut should return a CutNode one level deep if a CutNode larger than the node sent as an argument exists in its child list.", () => {
+        const cNodeChildCutLarger: CutNode = new CutNode(new Ellipse(testCenter, 10, 10));
+        cNode.child = cNodeChildCutLarger;
+
+        expect(cNode.getCurrentCut(jay)).toStrictEqual(cNodeChildCutLarger);
+    });
+
+    test("getCurrentCut should return the deepest CutNode that is still larger than the node sent as an argument.", () => {
+        const smallestButStillBiggerChild: CutNode = new CutNode(new Ellipse(testCenter, 8, 8));
+
+        const cNodeChildCutLarger = cNode.children[4] as CutNode;
+        cNodeChildCutLarger.child = new CutNode(new Ellipse(testCenter, 9, 9));
+        (cNodeChildCutLarger.children[0] as CutNode).child = smallestButStillBiggerChild;
+
+        expect(cNode.getCurrentCut(jay)).toStrictEqual(smallestButStillBiggerChild);
+    });
 });
 
 describe.skip("CutNode containsPoint soliloquy:", () => {
