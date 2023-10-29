103385d7c8dad30db35e512f1a76e18d4cafa055
DawnTheWitch <DawnTheWitchUwU@gmail.com>
2023-10-29 18:26:43 -0400

    It's broken

--- tests/UI/draw.spec.ts
+++ tests/UI/draw.spec.ts
@@ -14,7 +14,7 @@ test("help", async ({page}) => {
 });
 
 test("A or B", async ({page}) => {
-    await page.goto("https://rairlab.github.io/Peirce-My-Heart/");
+    await page.goto("http://127.0.0.1:3000");
 
     const canvas = page.locator("canvas");
     await page.getByTitle("Cut Mode").click();
@@ -42,6 +42,33 @@ test("A or B", async ({page}) => {
 
     await page.getByTitle("Atom Mode").press("B");
 
+    await canvas.click({
+        position: {x: 500, y: 500},
+    });
+
+    await expect(page.locator("#graphString")).toHaveText("[((A) (B))]");
+
+    const downloadPromise = page.waitForEvent("download");
+    await page.getByTitle("Save File").click();
+    const download = await downloadPromise;
+    await download.saveAs("./" + download.suggestedFilename());
+
+    //All of these should fail, and the canvas should remain the same.
+    await page.getByTitle("Cut Mode").click();
+
+    await canvas.dragTo(canvas, {
+        sourcePosition: {x: 300, y: 300},
+        targetPosition: {x: 700, y: 700},
+    });
+
+    await page.getByTitle("Atom Mode").click();
+
+    await page.getByTitle("Atom Mode").press("C");
+    await canvas.click({
+        position: {x: 200, y: 200},
+    });
+
+    await page.getByTitle("Atom Mode").press("D");
     await canvas.click({
         position: {x: 500, y: 500},
     });
