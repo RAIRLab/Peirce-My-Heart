316d10221bd5067e225cbf232346ba9927be6299
DawnTheWitch <DawnTheWitchUwU@gmail.com>
2023-10-29 12:56:25 -0400

    Intersection bug fixed, added basic comments

--- src/DrawModes/ResizeTool.ts
+++ src/DrawModes/ResizeTool.ts
@@ -3,13 +3,14 @@
  */
 
 import {Point} from "../AEG/Point";
+import {Ellipse} from "../AEG/Ellipse";
 import {AtomNode} from "../AEG/AtomNode";
 import {CutNode} from "../AEG/CutNode";
 import {tree} from "../index";
 import {offset} from "./DragMode";
 import {drawCut, redrawTree} from "./DrawUtils";
 import {legalColor, illegalColor} from "../Themes";
-import {alterCut} from "./EditModeUtils";
+import {ellipseLargeEnough} from "./CutMode";
 
 //The initial point the user pressed down.
 let startingPoint: Point;
@@ -20,6 +21,12 @@ let currentNode: CutNode | AtomNode | null = null;
 //Whether or not the node is allowed to be moved (not the sheet).
 let legalNode: boolean;
 
+/**
+ * Takes the point the user clicked and stores that for later use. If the lowest node containing
+ * that point is not the sheet, then store that as currentNode and find that node's parent.
+ * Removes the node from the parent and reinsert its children if it has any. Cannot be an Atom.
+ * @param event The event of a mouse down while using resize tool
+ */
 export function resizeMouseDown(event: MouseEvent) {
     startingPoint = new Point(event.x - offset.x, event.y - offset.y);
     currentNode = tree.getLowestNode(startingPoint);
@@ -37,47 +44,52 @@ export function resizeMouseDown(event: MouseEvent) {
     }
 }
 
+/**
+ * If the node is legal alters the center and both of the radii. Creates a copy of the current cut
+ * So that the original is not altered in any way.
+ * @param event The event of a mouse move while using the resize tool
+ */
 export function resizeMouseMove(event: MouseEvent) {
     if (legalNode) {
         const moveDifference: Point = new Point(
-            event.x - startingPoint.x,
-            event.y - startingPoint.y
+            (event.x - startingPoint.x) / 2,
+            (event.y - startingPoint.y) / 2
         );
 
         if (currentNode instanceof CutNode) {
-            const tempCut: CutNode = alterCut(currentNode, moveDifference);
+            const tempCut: CutNode = resizeCut(currentNode, moveDifference);
             //This is just to make the lint stop yelling
             if (tempCut.ellipse !== null) {
-                tempCut.ellipse.radiusX = tempCut.ellipse.radiusX + moveDifference.x;
-                tempCut.ellipse.radiusY = tempCut.ellipse.radiusY + moveDifference.y;
+                redrawTree(tree);
+                const legal = tree.canInsert(tempCut) && ellipseLargeEnough(tempCut.ellipse);
+                const color = legal ? legalColor() : illegalColor();
+                drawCut(tempCut, color);
             }
-
-            redrawTree(tree);
-            const color = tree.canInsert(tempCut) ? legalColor() : illegalColor();
-            drawCut(tempCut, color);
         }
     }
 }
 
+/**
+ * If the node is legal creates a new temporary cut and alters the ellipse center and radii.
+ * If this new cut can be inserted inserts that into the tree, otherwise reinserts the original.
+ * @param event The event of a mouse up while using the resize tool
+ */
 export function resizeMouseUp(event: MouseEvent) {
     if (legalNode) {
         const moveDifference: Point = new Point(
-            event.x - startingPoint.x,
-            event.y - startingPoint.y
+            (event.x - startingPoint.x) / 2,
+            (event.y - startingPoint.y) / 2
         );
 
         if (currentNode instanceof CutNode) {
-            const tempCut: CutNode = alterCut(currentNode, moveDifference);
+            const tempCut: CutNode = resizeCut(currentNode, moveDifference);
             //This is just to make the lint stop yelling
             if (tempCut.ellipse !== null) {
-                tempCut.ellipse.radiusX = tempCut.ellipse.radiusX + moveDifference.x;
-                tempCut.ellipse.radiusY = tempCut.ellipse.radiusY + moveDifference.y;
-            }
-
-            if (tree.canInsert(tempCut)) {
-                tree.insert(tempCut);
-            } else {
-                tree.insert(currentNode);
+                if (tree.canInsert(tempCut) && ellipseLargeEnough(tempCut.ellipse)) {
+                    tree.insert(tempCut);
+                } else {
+                    tree.insert(currentNode);
+                }
             }
         }
         redrawTree(tree);
@@ -85,10 +97,36 @@ export function resizeMouseUp(event: MouseEvent) {
     }
 }
 
+/**
+ * If the mouse leaves the canvas then it is no longer a legal node and reinserts the original.
+ */
 export function resizeMouseOut() {
     if (legalNode && currentNode !== null) {
         tree.insert(currentNode);
     }
     legalNode = false;
     redrawTree(tree);
+}
+
+/**
+ * Makes a copy of original cut and changes the center and radii by the difference given.
+ * @param originalCut The original cut that will be copied and altered
+ * @param difference The change for the new cut
+ * @returns The new altered cut
+ */
+function resizeCut(originalCut: CutNode, difference: Point) {
+    if (originalCut.ellipse !== null) {
+        return new CutNode(
+            new Ellipse(
+                new Point(
+                    originalCut.ellipse.center.x + difference.x - offset.x,
+                    originalCut.ellipse.center.y + difference.y - offset.y
+                ),
+                originalCut.ellipse.radiusX + difference.x,
+                originalCut.ellipse.radiusY + difference.y
+            )
+        );
+    } else {
+        throw new Error("Cannot alter the position of a cut without an ellipse.");
+    }
 }
