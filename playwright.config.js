839f04fe25a414d6f231aafb788a29049b641a16
AnushaTiwari5 <atiwari3@albany.edu>
2023-10-29 21:39:40 -0400

    possible web server setup?

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        //baseURL: 'http://127.0.0.1:3000',
+        baseURL: 'https://rairlab.github.io/Peirce-My-Heart/',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
@@ -69,9 +69,12 @@ export default defineConfig({
     ],
 
     //Run your local dev server before starting the tests
-    //webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    webServer: {
+        command: 'npm run server',
+        url: 'http://127.0.0.1:8081',
+        reuseExistingServer: !process.env.CI,
+        stdout: 'pipe',
+        stderr: 'pipe',
+        timeout: 60 * 1000,
+    },
 });
