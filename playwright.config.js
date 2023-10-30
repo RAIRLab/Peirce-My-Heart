39f6a5740b15426ff32a87ed5458a1d78f303d1f
AnushaTiwari5 <atiwari3@albany.edu>
2023-10-29 22:23:31 -0400

    worked on local system!

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://127.0.0.1',
+        baseURL: 'http://localhost:5173/Peirce-My-Heart/',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
@@ -71,7 +71,8 @@ export default defineConfig({
     //Run your local dev server before starting the tests
     webServer: {
         command: 'npm run server',
-        url: 'http://127.0.0.1',
+        //url: 'http://127.0.0.1',
+        port: 5173,
         reuseExistingServer: !process.env.CI,
         stdout: 'pipe',
         stderr: 'pipe',
