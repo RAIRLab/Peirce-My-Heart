7ff5128529b860c1de9ae4cfd2600639eef36b01
DawnTheWitch <DawnTheWitchUwU@gmail.com>
2023-11-26 11:36:01 -0500

    Deiteration working and a bit of cleanup added.

--- src/ProofTools/DeiterationTool.ts
+++ src/ProofTools/DeiterationTool.ts
@@ -33,20 +33,12 @@ export function deiterationMouseDown(event: MouseEvent) {
     currentProofTree = new AEGTree(treeContext.getLastProofStep().tree.sheet);
     currentNode = currentProofTree.getLowestNode(currentPoint);
 
-    if (currentNode !== null && !(currentNode instanceof CutNode && currentNode.ellipse === null)) {
-        legalNode = true;
-
-        if (canDeiterate(currentProofTree.sheet, currentProofTree.getLevel(currentNode))) {
-            highlightChildren(currentNode, illegalColor());
-        }
-    } else {
-        legalNode = false;
-    }
+    isLegal();
 }
 
 /**
- * Determines the lowest node containging the current point and if that is not the sheet it is
- * considered a legal node
+ * Determines the lowest node containing the current point and if that is not the sheet it is
+ * considered a legal node and will be highlighted.
  * @param event A mouse move event while using deiteration tool
  */
 export function deiterationMouseMove(event: MouseEvent) {
@@ -54,26 +46,17 @@ export function deiterationMouseMove(event: MouseEvent) {
     currentNode = currentProofTree.getLowestNode(currentPoint);
     redrawProof();
 
-    if (
-        legalNode &&
-        currentNode !== null &&
-        !(currentNode instanceof CutNode && currentNode.ellipse === null) &&
-        canDeiterate(currentProofTree.sheet, currentProofTree.getLevel(currentNode))
-    ) {
-        highlightChildren(currentNode, illegalColor());
-        legalNode = true;
-    } else {
-        legalNode = false;
-    }
+    isLegal();
 }
 
+/**
+ * If the node we currently have is legal, find it's parent and remove the current node from it.
+ * Push the new version of the tree onto the proof history array.
+ * @param event A mouse up event while using deiteration tool
+ */
 export function deiterationMouseUp(event: MouseEvent) {
-    //Stores the tree of the previous proof so that we can perform double cut actions without
-    //altering that tree
-    const currentPoint: Point = new Point(event.x - offset.x, event.y - offset.y);
-
     if (legalNode) {
-        currentNode = currentProofTree.getLowestNode(currentPoint);
+        const currentPoint: Point = new Point(event.x - offset.x, event.y - offset.y);
         if (
             currentNode !== null &&
             canDeiterate(currentProofTree.sheet, currentProofTree.getLevel(currentNode))
@@ -90,40 +73,73 @@ export function deiterationMouseUp(event: MouseEvent) {
     redrawProof();
 }
 
+/**
+ * Helper function to determine if the currently selected node is a legal node and highlight it.
+ */
+function isLegal() {
+    if (
+        currentNode !== null &&
+        !(currentNode instanceof CutNode && currentNode.ellipse === null) &&
+        canDeiterate(currentProofTree.sheet, currentProofTree.getLevel(currentNode))
+    ) {
+        highlightChildren(currentNode, illegalColor());
+        legalNode = true;
+    } else {
+        legalNode = false;
+    }
+}
+
+/**
+ * Reset the current null and make this node illegal until it's selected again, redraws the screen.
+ */
 export function deiterationMouseOut() {
     legalNode = false;
+    currentNode = null;
     redrawProof();
 }
 
-function canDeiterate(currentLevel: CutNode, level: number): boolean {
-    for (let i = 0; i < currentLevel.children.length; i++) {
+/**
+ * Searches the tree for an equal match to the node we're attempting to delete/deiterate that is
+ * not itself. It cannot go to a level lower than itself and it cannot search any cuts it is not
+ * contained by. The Node we search for is the currently selected no currentNode.
+ * @param currentParent The current node we are searching the children of
+ * @param level The level the node we're searching for is located
+ * @returns Whether or not this is a valid deiteration
+ */
+function canDeiterate(currentParent: CutNode, level: number): boolean {
+    let potentialParent: CutNode | null = null;
+    for (let i = 0; i < currentParent.children.length; i++) {
         //If both nodes are cuts, and they are equal then we have found a copy higher on the tree
         if (
-            currentLevel.children[i] instanceof CutNode &&
+            currentParent.children[i] instanceof CutNode &&
             currentNode instanceof CutNode &&
-            currentNode.isEqualTo(currentLevel.children[i] as CutNode) &&
-            currentNode.ellipse !== (currentLevel.children[i] as CutNode).ellipse
+            currentNode.isEqualTo(currentParent.children[i] as CutNode) &&
+            currentNode.ellipse !== (currentParent.children[i] as CutNode).ellipse
         ) {
             return true;
         } //If both nodes are atoms, and they both have the same identifier then they are equal
         else if (
-            currentLevel.children[i] instanceof AtomNode &&
+            currentParent.children[i] instanceof AtomNode &&
             currentNode instanceof AtomNode &&
-            (currentLevel.children[i] as AtomNode).identifier === currentNode.identifier &&
-            currentLevel.children[i].toString() !== currentNode.toString()
+            (currentParent.children[i] as AtomNode).identifier === currentNode.identifier &&
+            currentParent.children[i].toString() !== currentNode.toString()
         ) {
             return true;
+        } //If this cut has the node we're looking for we want to recurse towards it, however
+        //We want to still check the rest of this level so we do it afterwards.
+        else if (
+            currentParent.children[i] instanceof CutNode &&
+            (currentParent.children[i] as CutNode).containsNode(currentNode!) &&
+            currentProofTree.getLevel(currentParent.children[i]) < level
+        ) {
+            potentialParent = currentParent.children[i] as CutNode;
         }
     }
 
-    for (let i = 0; i < currentLevel.children.length; i++) {
-        if (
-            currentLevel.children[i] instanceof CutNode &&
-            (currentLevel.children[i] as CutNode).containsNode(currentNode!) &&
-            currentProofTree.getLevel(currentLevel.children[i]) < level
-        ) {
-            return canDeiterate(currentLevel.children[i] as CutNode, level);
-        }
+    //If we did find the parent of the node we're trying to find an equal of, we look at the next
+    //closest node.
+    if (potentialParent !== null) {
+        return canDeiterate(potentialParent, level);
     }
 
     //If there was no equal found then we cannot deiterate.
