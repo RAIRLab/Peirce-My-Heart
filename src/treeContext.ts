f6adfef87b36ac132e46daf76ca6788599e61129
AnushaTiwari5 <atiwari3@albany.edu>
2023-12-01 19:57:47 -0500

    proof history maintained when toggling modes

--- src/treeContext.ts
+++ src/treeContext.ts
@@ -74,12 +74,21 @@ export class treeContext {
      * @param newStep The new proof node being added to the proof
      */
     public static pushToProof(newStep: ProofNode) {
-        if (this.currentProofStep !== this.proof[this.proof.length - 1]) {
-            const currentIndex: number = this.proof.indexOf(this.currentProofStep);
-            deleteButtons(currentIndex);
-            this.proof = this.proof.splice(0, currentIndex);
+        if (this.currentProofStep && this.proof.length > 0) {
+            //Compare the current step we are on and the last step stored in the history
+            //If they are not the same, we have moved back to a previous step and need to delete
+            //all the steps in between
+            if (
+                !this.currentProofStep.tree.isEqualTo(this.proof[this.proof.length - 1].tree) ||
+                this.currentProofStep.appliedRule !== this.proof[this.proof.length - 1].appliedRule
+            ) {
+                const currentIndex: number = this.proof.indexOf(this.currentProofStep);
+                deleteButtons(currentIndex);
+                this.proof = this.proof.splice(0, currentIndex);
+            }
         }
 
+        //Set the newest performed step as the current step, and it to the proof
         this.currentProofStep = newStep;
         this.proof.push(newStep);
         appendStep(newStep);
