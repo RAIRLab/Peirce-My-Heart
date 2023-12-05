ebcc27712e3d0a3ce0e66d736485dc1e1f8cfbed
Ryan Reilly <ryanreilly703@gmail.com>
2023-12-05 08:12:39 -0500

    Testing if the robots will enjoy this playwright.config.js - Tests now only run on Chromium playwright.yml - removed job for uploading test result artifact atom.spec.ts, loneAtom.json - seeing how these compare to what the robots expect

--- playwright.config.js
+++ playwright.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
             name: "chromium",
             use: {...devices["Desktop Chrome"]},
         },
-
+        /*
         {
             name: "firefox",
             use: {...devices["Desktop Firefox"]},
@@ -48,7 +48,7 @@ export default defineConfig({
             name: "webkit",
             use: {...devices["Desktop Safari"]},
         },
-
+        */
         /* Test against mobile viewports. */
         // {
         //   name: 'Mobile Chrome',
